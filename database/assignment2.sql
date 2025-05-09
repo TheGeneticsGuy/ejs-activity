ea40ec7f3e0d7d6d18ba21f3ec27da222bc0c296
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-05-09 13:01:14 -0700

    Part 4 and part 6 Copied over, part 6 working and run on Admin4

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -18,11 +18,11 @@ WHERE account_email = 'tony@starkent.com';
 -- Part 3
 DELETE FROM public.account
 WHERE account_email = 'tony@starkent.com';
--- Part 4 - Start of where to copy over to rebuild file
+-- Part 4 - Copy This to Rebuild File
 UPDATE public.inventory
 SET inv_description = REPLACE(
         inv_description,
-        'small interiors',
+        'the small interiors',
         'a huge interior'
     )
 WHERE inv_make = 'GM'
@@ -34,4 +34,7 @@ SELECT inv.inv_make,
 FROM public.inventory inv
     INNER JOIN public.classification cls ON inv.classification_id = cls.classification_id
 WHERE cls.classification_name = 'Sport';
-- -
+-- PART 6 - update thumbnail path - Copy This to Rebuild File
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -234,3 +234,17 @@ VALUES (
         'White',
         5
     );
+-- Week 3 assignment
+-- Part 4
+UPDATE public.inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_make = 'GM'
+    AND inv_model = 'Hummer';
+-- Part 6
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
