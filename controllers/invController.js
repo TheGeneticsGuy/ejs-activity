129973425cb31d54f9d81bb04da2155c3f86b73f
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-05-24 02:58:26 -0700

    Updated the Inventory view CSS - working on detail view

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,4 +19,30 @@ invCont.buildByClassificationId = async function (req, res, next) {
     })
 }
 
+/* ***************************
+ *  Build inventory item detail view
+ *  Example: /inv/detail/5
+ * ************************** */
+invCont.buildByInventoryId = async function (req, res, next) {
+    const inventory_id = req.params.inventoryId;
+    const itemData = await invModel.getInventoryByInventoryId(inventory_id);
+
+    if (!itemData) {
+        const err = new Error("Vehicle not found.");
+        err.status = 404;
+        return next(err); // This is so the global error handler in server.js can handle it for my 404 page we made
+    }
+
+    const detailViewHtml = await utilities.buildInventoryDetailView(itemData); // You'll create this utility function
+    let nav = await utilities.getNav();
+    const vehicleName = `${itemData.inv_make} ${itemData.inv_model}`;
+
+    res.render("./inventory/detail", { // You'll create this EJS template
+        title: vehicleName, // For the <title> tag and potentially an <h1> in layout
+        nav,
+        detailContent: detailViewHtml, // The main HTML content for the detail view
+        errors: null,
+    });
+}
+
 module.exports = invCont
