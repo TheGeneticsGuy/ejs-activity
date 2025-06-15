8c30fb70d2f2bdee5069ff68243c0c19eb043f1d
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-06-14 23:06:03 -0700

    Fixed a registration  issue

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = new express.Router();
-const accController = require("../controllers/accountController");
+const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/");
 
 // Making a login page
-router.get("/login", utilities.handleErrors(accController.buildLogin));
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 // Making a registration page
-router.get("/register", utilities.handleErrors(accController.buildRegister));
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Route to process registration
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
