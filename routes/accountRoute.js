f90a5c89fce39b91126dc39882d1a12034ff5f74
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-06-14 23:49:17 -0700

    Validation messages styled

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,14 +2,19 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/");
+const regValidate = require('../utilities/account-validation')
 
 // Making a login page
-router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/login",utilities.handleErrors(accountController.buildLogin));
 
 // Making a registration page
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Route to process registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post('/register',
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
